5fe61970a81c28dae40c3f51a0156e7de56693ec
jakehart <jacobdhart@gmail.com>
2017-01-10 13:50:49 -0600

    Added a pause button to both games.

---
+++ Assets/Scripts/PauseOnClick.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseOnClick : MonoBehaviour
+{
+    private bool m_isPaused = false;
+
+    void Start()
+    {
+        var button = GetComponent<Button>();
+        if (!button)
+        {
+            Debug.LogError("Missing button component", gameObject);
+            enabled = false;
+            return;
+        }
+
+        button.onClick.AddListener(OnClick);
+    }
+
+    void OnDestroy()
+    {
+        var button = GetComponent<Button>();
+        if (button)
+            button.onClick.RemoveListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        m_isPaused = !m_isPaused;
+
+        Time.timeScale = m_isPaused ? 0f : 1f;
+        GetComponent<Image>().color = m_isPaused ? Color.red : Color.white;
+    }
+}
