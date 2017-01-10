007eadcb0d8cff3f4f1f220486c313d20d9a71e2
jakehart <jacobdhart@gmail.com>
2017-01-10 12:28:34 -0600

    Simplified raycasting from the AR camera. ARCam now properly releases gyro control when we're using mouselook.

--- Assets/Scripts/ARCam.cs
+++ Assets/Scripts/ARCam.cs
@@ -2,14 +2,9 @@
 using System.Collections;
 
 public class ARCam : MonoBehaviour {
-	public int top;
-	public int side;
-
-    private Camera cam;
+    private bool m_isUsingGyro = true;
 
     void Start () {
-		/// for the ray trace from camera
-		cam = GetComponent<Camera>();
 		/// for movement in real space
 		Input.gyro.enabled = true;
 
@@ -19,6 +14,7 @@ public class ARCam : MonoBehaviour {
         if (!SystemInfo.supportsGyroscope && !UnityEditor.EditorApplication.isRemoteConnected)
         {
             gameObject.AddComponent<SimpleSmoothMouseLook>();
+            m_isUsingGyro = false;
         }
 #endif
     }
@@ -26,16 +22,17 @@ public class ARCam : MonoBehaviour {
 	void Update () {
 		Quaternion rotation = new Quaternion();
 
-        Vector3 angles = Input.gyro.attitude.eulerAngles;
-        rotation.eulerAngles = new Vector3(-angles.x, -angles.y, angles.z);
-        transform.rotation = rotation;
+        if (m_isUsingGyro)
+        {
+            Vector3 angles = Input.gyro.attitude.eulerAngles;
+            rotation.eulerAngles = new Vector3(-angles.x, -angles.y, angles.z);
+            transform.rotation = rotation;
+        }
 
 		//ray from the camera for detenction of objects
-		top  = cam.pixelWidth / 2;
-		side = cam.pixelHeight / 2;
 		RaycastHit hit;
-		Ray ray = GetComponent<Camera>().ScreenPointToRay(new Vector3(top, side, 0));
-		Debug.DrawRay(ray.origin, ray.direction * 84, Color.yellow);
+        var ray = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * 84, Color.yellow);
 
 		if (Physics.Raycast (ray, out hit))
 		{
