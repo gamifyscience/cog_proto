aaf6f0ad8fbdac489bb72edde3323e5316e62e02
jakehart <jacobdhart@gmail.com>
2017-01-11 12:34:10 -0600

    Spawn bugs in random positions near the camera.

---
+++ Assets/Scripts/BugSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugSpawner : MonoBehaviour
+{
+    public GameObject m_bugPrefab;
+    public int kMinBugCount = 2;
+    public int kMaxBugCount = 6;
+
+    private void Awake()
+    {
+        int numBugs = Random.Range(kMinBugCount, kMaxBugCount);
+
+        // Spawn some bugs near the world origin.
+        for (int i = 0; i < numBugs; ++i)
+        {
+            Vector3 pos = RandomVector3(-10f, 10f);
+
+            SpawnBug(pos);
+        }
+    }
+
+    private static Vector3 RandomVector3(float min, float max)
+    {
+        return new Vector3(
+            Random.Range(min, max),
+            Random.Range(min, max),
+            Random.Range(min, max)
+            );
+    }
+
+    // Spawns a single bug.
+    private void SpawnBug(Vector3 position)
+    {
+        GameObject bug = Instantiate(m_bugPrefab, position, Quaternion.identity) as GameObject;
+    }
+}
